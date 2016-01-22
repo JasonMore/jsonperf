42f50c871e97b75fbb959d704c895e36aab4c48a
Jason More <Jason.More@gmail.com>
2016-01-22 14:39:01 -0600

    refactored node to copy groovy way

--- node/index.js
+++ node/index.js
@@ -4,27 +4,16 @@ var json = fs.readFileSync('../cities.json');
 var times = [];
 
 for (var i = 0; i < 20; i++){
-	var ts = parseJson(json);
-	times.push(ts);
-}
-
-console.log("average: " + average(times));
-
-function parseJson(json){
 	var stopWatch = process.hrtime();
 	var cities = JSON.parse(json);
+	var duration = process.hrtime(stopWatch);
+
+	var ms = (duration[0] * 1e9 + duration[1]) * 1e-6;
+
 	console.log("Number of cities: " + cities.length);
-	var ts = process.hrtime(stopWatch);
-	var nanoseconds = ts[0] * 1e9 + ts[1];
-	var ms = nanoseconds * 1e-6;
-	console.log(ms, "RunTime")
-	return ms;
+	console.log(ms, "RunTime");
+	times.push(ms);
 }
 
-function average(arr){
-	var sum = 0;
-	for (var i = 0; i < arr.length; i++) {
-		sum += arr[i];
-	}
-	return sum / arr.length;
-}
+var sum = times.reduce((sum, val) => sum + val);
+console.log("average: " +  sum / times.length);
